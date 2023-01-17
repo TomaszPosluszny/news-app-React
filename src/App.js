ef5e62a8fe78a6c5a049be909b19ee2458c82774
TomaszPosluszny <tomkposluszny@gmail.com>
2023-01-17 18:17:01 +0100

    add class component name AddNews

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './App.css';
 
+class AddNews extends React.Component {
+	render() {
+		const { name, power, code } = this.props;
+		return (
+			<div className='News'>
+				<h2>{name}</h2>
+				<p>{power}</p>
+				<p>{code}</p>
+			</div>
+		);
+	}
+}
+
 function News({ name, power, code }) {
 	return (
-		<div>
+		<div className='News'>
 			<h2>{name}</h2>
 			<p>{power}</p>
 			<p>{code}</p>
@@ -13,29 +26,29 @@ function News({ name, power, code }) {
 
 const data = [
 	{
-		name: 'Haier',
+		name: 'Rotenso',
 		power: '5 kW',
-    code: 'B235'
+		code: 'B235',
 	},
-  {
+	{
 		name: 'Haier',
 		power: '3,5 kW',
-    code: 'B235'
+		code: 'B235',
 	},
-  {
+	{
 		name: 'Lg',
 		power: '2,5 kW',
-    code: 'C654'
+		code: 'C654',
 	},
-
 ];
 
 function App() {
 	return (
 		<div className='App'>
-      {data.map((elem) =>{
-      return <News name={elem.name} power={elem.power} code={elem.code} />
-  })}
+			{data.map((elem) => {
+				return <div><News name={elem.name} power={elem.power} code={elem.code} />
+				<AddNews name={elem.name} power={elem.power} code={elem.code} /></div>;
+			})}
 		</div>
 	);
 }
