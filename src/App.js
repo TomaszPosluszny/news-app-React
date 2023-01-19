fd0380c2d13cdc2e8787bb718d269eef3b5205b2
TomaszPosluszny <tomkposluszny@gmail.com>
2023-01-19 18:47:40 +0100

    add state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 class AddNews extends React.Component {
+	state = {
+		date: new Date(),
+		score: 5,
+	};
+	componentDidMount() {
+		this.setState({ score: 8 });
+	}
+
 	render() {
 		const { name, power, code } = this.props;
+		const { score } = this.state;
 		return (
 			<div className='News'>
 				<h2>{name}</h2>
 				<p>{power}</p>
 				<p>{code}</p>
+				<p>{score}</p>
 			</div>
 		);
 	}
 }
 
 function News({ name, power, code }) {
+	const [scoreAdd, setScore] = useState(6);
 	return (
 		<div className='News'>
 			<h2>{name}</h2>
 			<p>{power}</p>
 			<p>{code}</p>
+			<p>{scoreAdd}</p>
 		</div>
 	);
 }
@@ -46,8 +58,12 @@ function App() {
 	return (
 		<div className='App'>
 			{data.map((elem) => {
-				return <div><News name={elem.name} power={elem.power} code={elem.code} />
-				<AddNews name={elem.name} power={elem.power} code={elem.code} /></div>;
+				return (
+					<div>
+						<News name={elem.name} power={elem.power} code={elem.code} />
+						<AddNews name={elem.name} power={elem.power} code={elem.code} />
+					</div>
+				);
 			})}
 		</div>
 	);
