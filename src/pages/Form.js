f52b83aac88ce866b33ee8c5e31bb390cefbf12d
TomaszPosluszny <tomkposluszny@gmail.com>
2023-02-02 15:05:04 +0100

    add state

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,21 +1,73 @@
-import React from "react";
+import React from 'react';
 
+class Form extends React.Component {
+	state = {
+		name: '',
+		surname: '',
+		age: '',
+	};
 
-function Form() {
-
-    const handleClick = () => console.log('React Button')
-    const handleSubmit = (event) => {
-        event.preventDefault ()
-    console.log(event)
+	handleSubmit(event) {
+		event.preventDefault();
+	}
+	handleClick = (event) => {
+		console.log('handleClick: ', event);
+		console.log('React Button');
+	};
+	handleChange = (event) => {
+		const keyName = event.target.name;
+		this.setState({ [keyName]: event.target.value });
+	};
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit.bind(this)}>
+				<div>
+					<input
+						type='text'
+						name='name'
+						placeholder='Name'
+						onChange={this.handleChange}
+					/>
+				</div>
+				<div>
+					<input
+						type='text'
+						name='surname'
+						placeholder='Surname'
+						onChange={this.handleChange}
+					/>
+				</div>
+				<div>
+					<input
+						type='text'
+						name='age'
+						placeholder='Age'
+						onChange={this.handleChange}
+					/>
+				</div>
+				<button type='submit' onClick={this.handleClick}>
+					Send
+				</button>
+			</form>
+		);
+	}
 }
-    return(
-        <form onSubmit={handleSubmit}>
-            <button onClick={handleClick}>
-React Button
-            </button>
-            </form>
-    )
-    
-}
 
-export default Form
+// function Form() {
+
+//     const handleClick = () => console.log('React Button')
+//     const handleSubmit = (event) => {
+//         event.preventDefault ()
+//     console.log(event)
+// }
+//     return(
+//         <form onSubmit={handleSubmit}>
+//             <button onClick={handleClick}>
+// React Button
+//             </button>
+//             </form>
+//     )
+
+// }
+
+export default Form;
